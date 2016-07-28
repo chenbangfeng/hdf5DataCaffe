f4818cfa2fe5e5667bbcdfbac861b9973948fe3d
Ines Chami <ines.chami@cea.fr>
2016-07-28 10:59:23 +0200

    added float data hdf5 files

---
+++ create_hdf5_file_float_data.py
@@ -0,0 +1,32 @@
+import h5py, sys
+import numpy as np
+
+SIZE = 4096
+NUM_LABELS = 500
+
+
+# *******************************************************************************************
+
+
+lines = open(sys.argv[1], 'r').readlines()
+mode = sys.argv[2]
+
+# If you do not have enough memory split data into
+# multiple batches and generate multiple separate h5 files
+data = np.zeros((len(lines), SIZE), dtype='f4')
+labels = np.zeros((len(lines), NUM_LABELS), dtype='f4')
+num_data = len(lines)
+for i, line in enumerate(lines):
+    line = line.rstrip().split(' ')
+    vgg = line[:4096]
+    if mode == 'train':
+        lab = line[4096:]
+        labels[i] = [float(x) for x in lab]
+    data[i] = [float(x) for x in vgg]
+
+with h5py.File(sys.argv[1]+'.h5', 'w') as H:
+    H.create_dataset('data', data=data)
+    if mode == 'train':
+        H.create_dataset('label', data=labels)
+
+# *******************************************************************************************
